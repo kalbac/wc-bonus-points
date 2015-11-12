8d8876fb7747e6028d2d8e773b604a289f81a90e
kalbac <maksim@martirosoff.ru>
2015-11-12 18:48:55 +0300

    Create main file

---
+++ woocommerce-bonus-points.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Bonus Points
+Plugin URI: http://woodev.ru
+Description: Adding bonus points system into customer profile.
+Version: 1.0
+Author: Maksim Martirosov
+Author URI: http://martirosoff.ru
+Project: wc-bonus-points
+License: A "Slug" license name e.g. GPL2
+*/
