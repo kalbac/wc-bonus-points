1238af0ffedbfee0c6cbac72c265a86c278b49c3
kalbac <maksim@martirosoff.ru>
2015-11-13 20:53:58 +0300

    1.1 version release

--- woocommerce-bonus-points.php
+++ woocommerce-bonus-points.php
@@ -4,7 +4,7 @@
  * Plugin Name: Bonus Points
  * Plugin URI: http://woodev.ru
  * Description: Adding bonus points system into customer profile.
- * Version: 1.0.2
+ * Version: 1.1.0
  * Author: Maksim Martirosov
  * Author URI: http://martirosoff.ru
  * Project: wc-bonus-points
